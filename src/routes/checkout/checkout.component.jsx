5c982bf32322f8f68033d6b46409c6c42f4b9f2b
nmehanma <nmehanma@gmail.com>
2022-05-19 00:54:45 -0400

    integrate total calculaton and rendering of items in checkout

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,8 +7,7 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems } =
-    useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
   return (
     <div className="checkout-container">
       <div className="checkout-header">
@@ -28,10 +27,10 @@ const Checkout = () => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map((cartItem) => 
+      {cartItems.map((cartItem) => (
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      )}
-      <span className="total">Total: 0</span>
+      ))}
+      <span className="total">Total: ${cartTotal}</span>
     </div>
   );
 };
