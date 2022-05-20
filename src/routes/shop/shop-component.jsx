7c2eb1e27fb07f7666573be6e05191d7acd8c62e
nmehanma <nmehanma@gmail.com>
2022-05-19 20:37:27 -0400

    nesting routes shop and fixing styling of main shop page

--- src/routes/shop/shop-component.jsx
+++ src/routes/shop/shop-component.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 
@@ -7,7 +8,8 @@ const Shop = () => {
   // console.log(products[0].id);
   return (
     <Routes>
-      <Route index element={CategoriesPreview} />
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
